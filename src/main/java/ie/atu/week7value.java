c019c8acdb529f6e14f87152979c83d93e292422
g00402163 <g00402163@atu.ie>
2022-11-07 10:08:54 +0000

    value between 0 and 9

---
+++ src/main/java/ie/atu/week7value.java
@@ -0,0 +1,28 @@
+package ie.atu;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class week7value
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Please enter a number between 0 - 9 :");
+        Scanner myscan = new Scanner(System.in);
+        try
+        {
+            int myNum = myscan.nextInt();
+
+             if (myNum <= 9 && myNum > 0)
+             {
+                 System.out.println("You entered " + myNum);
+             } else
+             {
+                  System.out.println("Not a valid number");
+        }
+             }
+        catch(InputMismatchException f)
+        {
+            System.out.println("invalid");
+        }
+    }
+}
